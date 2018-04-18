844d651cee312fd76fc3fe0b6ee8177fc9ea0c42
Aaditya Naik <aadi.naik@gmail.com>
2018-04-18 14:50:37 +0530

    Renamed the parser files to command

---
+++ shell/iomodule/command.cpp
@@ -0,0 +1,50 @@
+#include "command.hpp"
+
+#include <sstream>
+
+namespace io {
+	Command::Command(std::string line) {
+		std::stringstream lineStream(line);
+
+		unsigned int countArgs = 0;
+		for (char c : line) {
+			if (isspace(c)) {
+				countArgs++;
+			}
+		}
+
+		numArgs = countArgs;
+
+		args = new std::string[numArgs];
+
+		lineStream >> commd;
+
+		int i = 0;
+
+		while (lineStream >> args[i]) {
+			i++;
+			if (i >= numArgs) {
+				break;
+			}
+		}
+	}
+
+	std::string Command::getString() {
+		std::string string = "";
+		string += "Command- " + commd + "\nArgs- ";
+		for (int i = 0; i < numArgs; i++) {
+			string += args[i] + " ";
+		}
+
+		return string;
+	}
+
+	std::string Command::execute() {
+		std::string string = "";
+		return string;
+	}
+
+	Command parse(std::string line) {
+		return Command(line);
+	}
+}
---
+++ shell/iomodule/command.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <string>
+#include <map>
+#include <array>
+
+namespace io {
+	class Command {
+	private:
+		// format is command and args
+		std::string commd;
+		std::string* args;
+		unsigned int numArgs;
+
+		std::map< std::string, std::array<int, 2> > definedCommands = { { "exit", std::array<int, 2>{0, 0} },{ "echo", std::array<int, 2>{-1, 0} },
+																		{ "who", std::array<int, 2>{0, 0} },{ "help", std::array<int, 2>{0, 0} } };
+		// in the map, the string denotes the commands while the array of integers it is mapped to denotes the number of arguments needed and whether
+		// it needs the processor to execute the commands ( 1 if true )
+
+	public:
+		// parameterized constructor
+		Command(std::string);
+
+		std::string getString();
+
+		std::string execute();
+	};
+
+	Command parse(std::string line);
+
+	// command definitions
+	std::string echo(std::string strToEcho);
+	void exit();
+	std::string who();
+	std::string help();
+}
