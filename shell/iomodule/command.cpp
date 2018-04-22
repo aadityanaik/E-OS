6f9f5ae27d0fa4233a1903583fdf4d062bcaed88
Aaditya Naik <aadi.naik@gmail.com>
2018-04-22 20:45:16 +0530

    Added arithmetic commands to shell

--- shell/iomodule/command.cpp
+++ shell/iomodule/command.cpp
@@ -74,8 +74,47 @@ namespace io {
 				return cow();
 			}
 		}
+		else if (commd == "add") {
+			return add(args, numArgs);
+		}
+		else if (commd == "sub") {
+			return subtract(args, numArgs);
+		}
+		else if (commd == "mult") {
+			return multiply(args, numArgs);
+		}
+		else if (commd == "div") {
+			return divide(args, numArgs);
+		}
 		else {
-			return "\'" + commd + "\': no such command";
+			// Will require process management module
+
+			int commdCode = 0;
+			// denotes the command code
+
+			if (commd == "roundrobin") {
+				commdCode = -1;
+			}
+			else if (commd == "lf") {
+				commdCode = 1;
+			}
+			else if (commd == "fat") {
+				commdCode = 2;
+			}
+			else if (commd == "read") {
+				commdCode = 3;
+			}
+			else if (commd == "append") {
+				commdCode = 4;
+			}
+			else if (commd == "write") {
+				commdCode = 5;
+			}
+			else {
+				return "\'" + commd + "\': no such command";
+			}
+
+			return "";
 		}
 	}
 
@@ -106,5 +145,103 @@ namespace io {
 	std::string cow() {
 		std::string cowString = "^__^\n(oo)\_______\n(__)\       )\\ / \\\n   ||----w |\n   ||     ||";
 		return cowString;
+	}
+
+	std::string add(std::string* args, int numArgs) {
+		if (numArgs == 0) {
+			return "0";
+		}
+		else {
+			double argDouble = 0;
+			double	 sum = 0;
+			for (int i = 0; i < numArgs; i++) {
+				try {
+					argDouble = std::stod(args[i]);
+					sum += argDouble;
+				}
+				catch (...) {
+					return "\'" + args[i] + "\': invalid input";
+				}
+			}
+
+			return std::to_string(sum);
+		}
+	}
+
+	std::string subtract(std::string* args, int numArgs) {
+		if (numArgs == 0) {
+			return "0";
+		}
+		else if (numArgs == 1) {
+			return args[0];
+		}
+		else if (numArgs == 2) {
+			std::string s;
+			try {
+				s = args[0];
+				double a = std::stod(s);
+				s = args[1];
+				double b = std::stod(s);
+
+				return std::to_string(a - b);
+			}
+			catch (...) {
+				return "\'" + s + "\': invalid input";
+			}
+		}
+		else {
+			return "\'sub\': too many arguments";
+		}
+	}
+
+	std::string multiply(std::string* args, int numArgs) {
+		if (numArgs == 0) {
+			return "0";
+		}
+		else {
+			double argDouble = 0;
+			double	 sum = 1;
+			for (int i = 0; i < numArgs; i++) {
+				try {
+					argDouble = std::stod(args[i]);
+					sum *= argDouble;
+				}
+				catch (...) {
+					return "\'" + args[i] + "\': invalid input";
+				}
+			}
+
+			return std::to_string(sum);
+		}
+	}
+
+	std::string divide(std::string* args, int numArgs) {
+		if (numArgs == 0) {
+			return "0";
+		}
+		else if (numArgs == 1) {
+			return args[0];
+		}
+		else if (numArgs == 2) {
+			std::string s;
+			try {
+				s = args[0];
+				double a = std::stod(s);
+				s = args[1];
+				double b = std::stod(s);
+
+				if (b == 0) {
+					return "\'div\': cannot divide by 0";
+				}
+
+				return std::to_string(a / b);
+			}
+			catch (...) {
+				return "\'" + s + "\': invalid input";
+			}
+		}
+		else {
+			return "\'div\': too many arguments";
+		}
 	}
 }
--- shell/iomodule/command.hpp
+++ shell/iomodule/command.hpp
@@ -10,7 +10,8 @@ namespace io {
 
 	const std::map< std::string, std::string > definedCommands = { { "exit", "Exits the shell, shutting down the OS" },{ "echo", "Echos your arguments back at you" },
 																	{ "who", "Tells you who you are" },{ "help", "Displays all inbuilt commands and their description" },
-																	{"cow", "Shows an ASCII cow"} };
+																	{"cow", "Shows an ASCII cow"}, {"add", "Adds the arguments together"}, {"sub", "Subtracts argument 2 from argument 1"},
+																	{"mult", "Multiplies all arguments"}, {"div", "Divides argument 1 by argument 2"} };
 	// in the map, the first string denotes commands while the second their description
 
 	class Command {
@@ -38,4 +39,10 @@ namespace io {
 	std::string who(Shell& shell);
 	std::string help();
 	std::string cow();
+
+	// arithmetic commands
+	std::string add(std::string*, int);
+	std::string subtract(std::string*, int);
+	std::string multiply(std::string*, int);
+	std::string divide(std::string*, int);
 }
